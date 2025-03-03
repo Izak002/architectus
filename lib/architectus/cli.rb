3cabc03b8433ef134d61e8e91e82b38e1192c117
Izak <izaklerouxx02@gmail.com>
2025-03-03 15:38:58 +0200

    Add banner and setup for ruby flow

--- lib/architectus/cli.rb
+++ lib/architectus/cli.rb
@@ -3,6 +3,16 @@ module Architectus
   class CLI < Thor
     include Thor::Actions
 
+    # Method to check if 'figlet' and 'lolcat' is installed and display the banner if so
+    def self.show_banner
+      # Check if 'figlet' and 'loclat' is available on the system
+      if system("which figlet > /dev/null 2>&1") && system("which lolcat > /dev/null 2>&1")
+        system("figlet Architectus | lolcat")
+      else
+        puts "Figlet or lolcat is not installed, skipping banner."
+      end
+    end
+
     # creates aliases/shortcuts for commands
     map %w[--version -v] => :version
     map %w[--help -h] => :help
@@ -16,6 +26,7 @@ module Architectus
 
     desc 'version', 'Display Architectus version'
     def version
+      self.class.show_banner
       pastel = Pastel.new
       puts pastel.cyan.bold("Architectus v#{Architectus::VERSION}")
     end
@@ -23,13 +34,43 @@ module Architectus
     desc 'new PROJECT_NAME', 'Create a new project with the specified structure'
     method_option :language, aliases: '-l', type: :string, default: 'ruby', desc: 'Programming language to scaffold'
     def new(project_name)
+      self.class.show_banner
+
+      pastel = Pastel.new
+      spinner = TTY::Spinner.new("[:spinner] Creating #{pastel.cyan(project_name)} project... :title", format: :dots)
 
       print_welcome_box(project_name, options[:language])
+
+      # create project
+      begin
+        spinner.auto_spin
+
+        spinner.update(title: "Getting the appropriate language scaffolder")
+        scaffolder = get_scaffolder(options[:language])
+        
+
+        # Print success message
+        puts ""
+        puts pastel.green.bold("✅ Project created successfully!")
+        puts pastel.cyan("Your #{options[:language].capitalize} project is ready in ./#{project_name}")
+        puts ""
+        puts "Next steps:"
+        puts pastel.yellow("  cd #{project_name}")
+        scaffolder.next_steps.each do |step|
+          puts pastel.yellow("  #{step}")
+        end
+      rescue StandardError => e
+        puts pastel.red("Failed to create project: #{e.message}")
+        exit(1)
+      end
+      
       
     end
 
     desc 'list', 'List all supported programming languages'
     def list
+      self.class.show_banner
+
       pastel = Pastel.new
       puts pastel.cyan.bold("Supported Languages:")
       puts pastel.green("  • Ruby")
@@ -38,6 +79,15 @@ module Architectus
     end
 
     private
+
+    def get_scaffolder(language)
+      case language.downcase
+      when 'ruby'
+        Architectus::Languages::Ruby.new
+      else
+        raise "Unsupported language: #{language}"
+      end
+    end
 
     def print_welcome_box(project_name, language)
       pastel = Pastel.new
