80700c614ff339f4214c4daad5059fa75c2846ed
Izak <izaklerouxx02@gmail.com>
2025-03-03 13:16:26 +0200

    Add version

--- Gemfile
+++ Gemfile
@@ -1,3 +1,4 @@
 source "https://rubygems.org"
 
 gem 'thor', '~> 1.3', '>= 1.3.2'
+gem 'pastel', '~> 0.8.0'
--- lib/architectus/cli.rb
+++ lib/architectus/cli.rb
@@ -1,7 +1,22 @@
 module Architectus
-  class CLI
-    def self.run
-      puts "hello world"
+  # Main CLI class for the language scaffold tool
+  class CLI < Thor
+    include Thor::Actions
+
+    # creates aliases/shortcuts for commands
+    map %w[--version -v] => :version
+    map %w[--help -h] => :help
+
+    # tells Thor to exit the program with a non-zero exit code whenever a command fails or raises an exception.
+    def self.exit_on_failure?
+      true
+    end
+
+    # commands
+    desc 'version', 'Display Architectus version'
+    def version
+      pastel = Pastel.new
+      puts pastel.cyan.bold("Architectus v#{Architectus::VERSION}")
     end
   end
 end
